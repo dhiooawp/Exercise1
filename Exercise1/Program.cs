248b0f8318048f2d616dbf528a36ada92ed6d594
DhioAWP <dhioanugerah0@gmail.com>
2023-04-05 16:50:22 +0700

    create Method void main and Membuat new program untuk login

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -10,6 +10,24 @@ namespace Exercise1
 {
     class Program
     {
-
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Koneksi Ke Database\n");
+                    Console.WriteLine("Masukkan User ID: ");
+                    string user = Console.ReadLine();
+                    Console.WriteLine("Masukkan Password: ");
+                    string pass = Console.ReadLine();
+                    Console.WriteLine("Masukkan Database tujuan: ");
+                    string db = Console.ReadLine();
+                    Console.Write("\nKetik K untuk terhubung ke Database: ");
+                    char chr = Convert.ToChar(Console.ReadLine());
+                }
+            }
+        }
     }
 }
