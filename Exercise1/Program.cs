568f5cf4fe6ac699e63f357bb3fe8b632a5afbf7
DhioAWP <dhioanugerah0@gmail.com>
2023-04-05 16:51:40 +0700

    Membuat pilihan dan case masing"

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -26,6 +26,91 @@ namespace Exercise1
                     string db = Console.ReadLine();
                     Console.Write("\nKetik K untuk terhubung ke Database: ");
                     char chr = Convert.ToChar(Console.ReadLine());
+                    switch (chr)
+                    {
+                        case 'K':
+                            {
+                                SqlConnection conn = null;
+                                string strKoneksi = "Data source = BRAINWASH\\DHIOAWP; " +
+                                    "initial catalog = {0}; " +
+                                    "User ID = {1}; password = {2}";
+                                conn = new SqlConnection(string.Format(strKoneksi, db, user, pass));
+                                conn.Open();
+                                Console.Clear();
+                                while (true)
+                                {
+                                    try
+                                    {
+                                        Console.WriteLine("\nMenu");
+                                        Console.WriteLine("1. Melihat seluruh data");
+                                        Console.WriteLine("2. Tambah data");
+                                        Console.WriteLine("3. Keluar");
+                                        Console.WriteLine("\nEnter your choice (1-3): ");
+                                        char ch = Convert.ToChar(Console.ReadLine());
+                                        switch (ch)
+                                        {
+                                            case '1':
+                                                {
+                                                    Console.Clear();
+                                                    Console.WriteLine("Data Member\n");
+                                                    Console.WriteLine();
+                                                    pr.baca(conn);
+                                                    conn.Close();
+                                                }
+                                                break;
+                                            case '2':
+                                                {
+                                                    Console.Clear();
+                                                    Console.WriteLine("Input Data Member\n");
+                                                    Console.WriteLine("Masukkan Id Member :");
+                                                    string NIM = Console.ReadLine();
+                                                    Console.WriteLine("Masukkan Nama Member :");
+                                                    string NmaMhs = Console.ReadLine();
+                                                    Console.WriteLine("Masukkan Alamat Member :");
+                                                    string Almt = Console.ReadLine();
+                                                    Console.WriteLine("Masukkan No Telepon :");
+                                                    string notlpn = Console.ReadLine();
+                                                    try
+                                                    {
+                                                        pr.insert(NIM, NmaMhs, Almt, notlpn, conn);
+                                                        conn.Close();
+                                                    }
+                                                    catch
+                                                    {
+                                                        Console.WriteLine("\nAnda tidak memiliki " + "akses untuk menambah data");
+                                                    }
+                                                }
+                                                break;
+                                            case '3':
+                                                conn.Close();
+                                                return;
+                                            default:
+                                                {
+                                                    Console.Clear();
+                                                    Console.WriteLine("\nInvalid option");
+                                                }
+                                                break;
+                                        }
+                                    }
+                                    catch
+                                    {
+                                        Console.WriteLine("\nCheck for the value entered.");
+                                    }
+                                }
+                            }
+                        default:
+                            {
+                                Console.WriteLine("\nInvalid option");
+                            }
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Tidak dapat mengakses database menggunakan user tersebut\n");
+                    Console.ResetColor();
                 }
             }
         }
