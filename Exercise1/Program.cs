1d818687a55dc7162f58fda2f146c00cf113013b
DhioAWP <dhioanugerah0@gmail.com>
2023-04-05 16:47:23 +0700

    create class program

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+
+    }
+}
